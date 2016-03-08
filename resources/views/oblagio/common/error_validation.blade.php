3bcc4544e4cdd7136e1bfd43de8b6b85a80f6bcc
Muhamad Reza Abdul Rohim <reza.wikrama3@gmail.com>
2016-03-09 00:58:46 +0700

    conditional errors

--- resources/views/oblagio/common/error_validation.blade.php
+++ resources/views/oblagio/common/error_validation.blade.php
@@ -1,7 +1,9 @@
-@if(@$errors->any())
-    <div class="alert alert-danger">
-        @foreach(@$errors->all() as @$error)
-            <p>{{ @$error }}</p>
-        @endforeach
-    </div>
+@if(!empty($errors))
+	@if($errors->any())
+	    <div class="alert alert-danger">
+	        @foreach($errors->all() as $error)
+	            <p>{{ $error }}</p>
+	        @endforeach
+	    </div>
+	@endif
 @endif
