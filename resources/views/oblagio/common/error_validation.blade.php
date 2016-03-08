7691b6e908028399d92a212383d7aae1b265ccf4
Muhamad Reza Abdul Rohim <reza.wikrama3@gmail.com>
2016-03-09 00:56:31 +0700

    tes auth

--- resources/views/oblagio/common/error_validation.blade.php
+++ resources/views/oblagio/common/error_validation.blade.php
@@ -1,7 +1,7 @@
-@if($errors->any())
+@if(@$errors->any())
     <div class="alert alert-danger">
-        @foreach($errors->all() as $error)
-            <p>{{ $error }}</p>
+        @foreach(@$errors->all() as @$error)
+            <p>{{ @$error }}</p>
         @endforeach
     </div>
 @endif
--- resources/views/oblagio/layouts/header.blade.php
+++ resources/views/oblagio/layouts/header.blade.php
@@ -1,3 +1,9 @@
+<?php
+  function user()
+  {
+    return Auth::user();
+  }
+?>
 <!DOCTYPE html>
 <html>
   <head>
