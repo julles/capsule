224970a50cc7a26b12fc58fb3222a95e168e75ab
Muhamad Reza Abdul Rohim <reza.wikrama3@gmail.com>
2016-03-09 22:16:37 +0700

    update routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,4 +14,20 @@ Route::group(['middleware' => ['web']], function () {
 	include __DIR__.'/oblagioRoutes.php';	
 });
 
+Route::group(['prefix' => og()->backendUrl , 'middleware' => ['web','auth','backend']],function(){
+		if(\Schema::hasTable('menus'))
+		{
+			foreach(og()->menu()->where('controller','!=','#')->get() as $row)
+			{
+				$controllerPath = app_path('Http/Controllers/'.$row->controller.'.php');
+
+				if(file_exists($controllerPath))
+				{
+					Route::controller($row->permalink,$row->controller);
+				}
+			}
+		}
+		
+});
+
 
